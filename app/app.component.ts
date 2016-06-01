2ca9615b763db6b068df8e230bdd8d37e8652249
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-06-01 14:53:46 -0700

    Add Component KegListComponent and Style click event to blue color where the user click on a list.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,51 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+
+@Component({
+  selector: 'keg-list',
+  inputs: ['kegList'],
+  outputs: ['onKegSelect'],
+  template: `
+  <div *ngFor="#currentKeg of kegList"
+    (click)="kegSelected(currentKeg)"
+    [class.selected]="currentKeg === selectedKeg">
+
+    <h3>{{ currentKeg.name }}</h3>
+    <ul>
+      <li>{{ "Brand: " + currentKeg.brand }}</li>
+      <li>{{ "Price: $" + currentKeg.price + " per keg"}}</li>
+      <li>{{ "ABV: " + currentKeg.alcoholContent + "%" }}</li>
+      <li>{{ "Pint servings: " + currentKeg.pintServings}}</li>
+    </ul>
+    
+  </div>
+  `
+})
+export class KegListComponent {
+  public kegList: Keg[];
+  public onKegSelect: EventEmitter<Keg>;
+  public selectedKeg: Keg;
+  constructor() {
+    this.onKegSelect = new EventEmitter();
+  }
+  kegSelected(clickedKeg: Keg): void {
+    console.log('child', clickedKeg);
+    this.selectedKeg = clickedKeg;
+    this.onKegSelect.emit(clickedKeg);
+  }
+}
 
 @Component({
   selector: 'my-app',
+  directives: [KegListComponent],
   template: `
     <div class="container">
       <h1>Tap Room</h1>
-      <div *ngFor="#keg of kegs">
-        <h3>{{ keg.name }}</h3>
-        <ul>
-          <li>{{ "Brand: " + keg.brand }}</li>
-          <li>{{ "Price: $" + keg.price + " per keg"}}</li>
-          <li>{{ "ABV: " + keg.alcoholContent + "%" }}</li>
-          <li>{{ "Pint servings: " + keg.pintServings}}</li>
-        </ul>
-      </div>
+
+      <keg-list
+        [kegList]="kegs"
+        (onKegSelect)="kegWasSelected($event)">
+      </keg-list>
+
     </div>
   `
 })
@@ -25,6 +57,9 @@ export class AppComponent {
       new Keg("Rise Up Red India Red Ale", "Hopworks", 180, 6.1, 1),
       new Keg("Head Hunter", "Fat Head", 170, 7.5, 2)
     ];
+  }
+  kegWasSelected(clickedKeg: Keg): void {
+    console.log('parent', clickedKeg);
   }
 }
 
