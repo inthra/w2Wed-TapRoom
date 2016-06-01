5511584ab838f91e4c17d33c885b9cb1d6520021
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-06-01 14:11:45 -0700

    Create Keg object. Display brand, name, price, ABV, and serving.

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,32 @@ import { Component } from 'angular2/core';
   template: `
     <div class="container">
       <h1>Tap Room</h1>
+      <div *ngFor="#keg of kegs">
+        <h3>{{ keg.name }}</h3>
+        <ul>
+          <li>{{ "Brand: " + keg.brand }}</li>
+          <li>{{ "Price: $" + keg.price + " per keg"}}</li>
+          <li>{{ "ABV: " + keg.alcoholContent + "%" }}</li>
+          <li>{{ "Pint servings: " + keg.pintServings}}</li>
+        </ul>
+      </div>
     </div>
   `
 })
-
 export class AppComponent {
+  public kegs: Keg[];
+  constructor(){
+    this.kegs = [
+      new Keg("Stumptown Tart Cherry Saison", "BridgePort", 175, 7, 0),
+      new Keg("Rise Up Red India Red Ale", "Hopworks", 180, 6.1, 1),
+      new Keg("Head Hunter", "Fat Head", 170, 7.5, 2)
+    ];
+  }
+}
 
+export class Keg {
+  public pintServings: number = 124;
+  constructor(public name: string, public brand: string, public price: number, public alcoholContent: number, public id: number) {
+
+  }
 }
