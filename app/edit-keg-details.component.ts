2269b75cff52a1313a8e15a332cce1b729cecc56
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-06-02 10:55:19 -0700

    Separate edit-keg-details into component and view parts for clearifing frontend and backend.

--- app/edit-keg-details.component.ts
+++ app/edit-keg-details.component.ts
@@ -4,15 +4,7 @@ import {Keg} from './keg.model';
 @Component({
   selector: 'edit-keg-details',
   inputs: ['keg'],
-  template: `
-  <div class="row">
-    <div class="keg-form">
-      <h3>Edit keg name: </h3>
-      <input [(ngModel)]="keg.name" class="col-sm-5 input-lg"/>
-      
-    </div>
-  </div>
-  `
+  templateUrl: 'app/edit-keg-details.view.html'
 })
 export class EditKegDetailsComponent {
   public keg: Keg;
---
+++ app/sell-one-pint.component.ts
@@ -0,0 +1,2 @@
+import {Component, EventEmitter} from 'angular2/core';
+import {Keg} from './keg.model';
