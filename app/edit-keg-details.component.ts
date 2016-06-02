b95b84dd9d55dd3a099dd7e16c6f411446a8643b
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-06-02 09:13:20 -0700

    Add edit keg name component. The Edit input field will be shown up when a user click on a keg list. Than the user can edit the name.

---
+++ app/edit-keg-details.component.ts
@@ -0,0 +1,18 @@
+import {Component} from 'angular2/core';
+import {Keg} from './keg.model';
+
+@Component({
+  selector: 'edit-keg-details',
+  inputs: ['keg'],
+  template: `
+  <div class="row">
+    <div class="keg-form">
+      <h3>Edit keg name: </h3>
+      <input [(ngModel)]="keg.name" class="col-sm-5 input-lg"/>
+    </div>
+  </div>
+  `
+})
+export class EditKegDetailsComponent {
+  public keg: Keg;
+}
